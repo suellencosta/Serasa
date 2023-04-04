d70258e2666c4be2305987483731b1239d238d0d
Suellen S. Costa <32553029+suellencosta@users.noreply.github.com>
2023-04-04 18:39:32 -0300

    Update comands.js

--- comands.js
+++ comands.js
@@ -1,51 +1,41 @@
-Cypress.Commands.add('createBoard', (boardName) => {
-    cy.request({
-      method: 'POST',
-      url: 'https://api.trello.com/1/boards/',
-      qs: {
-        name: boardName,
-        key: Cypress.env('TRELLO_API_KEY'),
-        token: Cypress.env('TRELLO_API_TOKEN')
-      }
-    }).then((response) => {
-      cy.wrap(response.body.id).as('boardId')
-    })
-  })
-  
-  Cypress.Commands.add('createCard', (cardName, listId) => {
-    cy.request({
-      method: 'POST',
-      url: 'https://api.trello.com/1/cards/',
-      qs: {
-        name: cardName,
-        idList: listId,
-        key: Cypress.env('TRELLO_API_KEY'),
-        token: Cypress.env('TRELLO_API_TOKEN')
-      }
-    }).then((response) => {
-      cy.wrap(response.body.id).as('cardId')
-    })
-  })
-  
-  Cypress.Commands.add('deleteCard', (cardId) => {
-    cy.request({
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('postRequest', (path, body) => {
+    return cy.request({
+        method: 'POST',
+        url: path,
+        qs: body
+      })
+});
+
+Cypress.Commands.add('deleteRequest', (path, body) => {
+    return cy.request({
       method: 'DELETE',
-      url: `https://api.trello.com/1/cards/${cardId}`,
-      qs: {
-        key: Cypress.env('TRELLO_API_KEY'),
-        token: Cypress.env('TRELLO_API_TOKEN')
-      }
+      url: path,
+      qs: body
     })
-  })
-  
-  Cypress.Commands.add('deleteBoard', (boardId) => {
-    cy.request({
-      method: 'DELETE',
-      url: `https://api.trello.com/1/boards/${boardId}`,
-      qs: {
-        key: Cypress.env('TRELLO_API_KEY'),
-        token: Cypress.env('TRELLO_API_TOKEN')
-      }
-    })
-  })
-  
+});  
